fb00d4e9fb4749d7c4710d920c3241e307c72b18
godfat <godfat@93678846-683e-0410-93c7-9b11d4a5ef47>
2008-01-23 05:51:46 +0000

    r1083@Macintosh:  godfat | 2008-01-23 13:51:14 +0800  * working in progress  * fixed sp_fwd.hpp.erb  * extract ChainChecker from Map  * added reverse_for_each
    git-svn-id: https://shooting-cubes.googlecode.com/svn@57 93678846-683e-0410-93c7-9b11d4a5ef47

--- trunk/lib/for_each_multi_array.hpp
+++ trunk/lib/for_each_multi_array.hpp
@@ -6,8 +6,16 @@ namespace psc{
 
 template <class MultiArray, class Fun>
 MultiArray& for_each(MultiArray& array, Fun fun){
-    typename MultiArray::element* data = array.data();
+    typename MultiArray::element* data = array.origin();
     for(int i=0, iend=array.num_elements(); i!=iend; ++i)
+        fun(data[i], i);
+    return array;
+}
+
+template <class MultiArray, class Fun>
+MultiArray& reverse_for_each(MultiArray& array, Fun fun){
+    typename MultiArray::element* data = array.origin();
+    for(int i=array.num_elements()-1, iend=-1; i!=iend; --i)
         fun(data[i], i);
     return array;
 }

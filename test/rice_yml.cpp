e35dff2579ee4ddafb74cabfaa170f41a4b776b4
godfat <godfat@93678846-683e-0410-93c7-9b11d4a5ef47>
2008-02-18 17:11:11 +0000

    r1921@220-135-28-18:  godfat | 2008-02-19 01:10:59 +0800  * test for rice using ruby yaml program.
    git-svn-id: https://shooting-cubes.googlecode.com/svn@117 93678846-683e-0410-93c7-9b11d4a5ef47

---
+++ test/rice_yml.cpp
@@ -0,0 +1,59 @@
+
+// g++ rice_yml.cpp -o rice_yml -I/opt/local/lib/ruby/1.8/i686-darwin9.1.0/ -I/opt/local/lib/ruby/gems/1.8/gems/rice-1.0.0/ -L/opt/local/lib/ruby/gems/1.8/gems/rice-1.0.0/rice/ -lrice -lruby -std=c++98 -Wall -w
+
+#include <ruby.h>
+#include <rice/Hash.hpp>
+#include <rice/Array.hpp>
+#include <iostream>
+
+using Rice::Object;
+using Rice::Class;
+using Rice::Hash;
+using Rice::Array;
+using Rice::String;
+using Rice::protect;
+
+void extract_hash(Hash const&);
+void extract_array(Array const& array){
+    Class hash_class(Hash().class_of());
+    Class array_class(Array().class_of());
+    Class string_class(String().class_of());
+    for(Array::const_iterator i=array.begin(), iend=array.end(); i!=iend; ++i){
+        if(i->is_instance_of(hash_class))
+            extract_hash(*i);
+        else if(i->is_instance_of(array_class))
+            extract_array(*i);
+        else if(i->is_instance_of(string_class))
+            std::cout << *i << "\n";
+        else
+            std::cout << *i << "\n";
+    }
+}
+
+void extract_hash(Hash const& hash){
+    Class hash_class(Hash().class_of());
+    Class array_class(Array().class_of());
+    Class string_class(String().class_of());
+    for(Hash::const_iterator i=hash.begin(), iend=hash.end(); i!=iend; ++i){
+        std::cout << i->key << ":\n";
+        Object obj(i->value);
+        if(obj.is_instance_of(hash_class))
+            extract_hash(obj);
+        else if(obj.is_instance_of(array_class))
+            extract_array(obj);
+        else if(obj.is_instance_of(string_class))
+            std::cout << i->value << "\n";
+        else
+            std::cout << i->value << "\n";
+    }
+}
+
+int main(){
+    ruby_init();
+    rb_eval_string("$LOAD_PATH << '/opt/local/lib/ruby/1.8'");
+    rb_eval_string("$LOAD_PATH << '/opt/local/lib/ruby/1.8/i686-darwin9.1.0'");
+    rb_require("yaml");
+    Hash h(protect(rb_eval_string, "YAML.load(File.read('database.yml'))"));
+    extract_hash(h);
+    std::cout << std::endl;
+}

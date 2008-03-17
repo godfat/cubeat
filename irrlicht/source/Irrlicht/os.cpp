dd438cfff23ca1a0011a782e53d96736848e4335
godfat <godfat@93678846-683e-0410-93c7-9b11d4a5ef47>
2008-03-17 05:40:38 +0000

    r2077@Macintosh:  godfat | 2008-03-17 13:39:52 +0800  * updated Makefile for mac os x!    just make and it would make Linux version but runs fine on Apple X11.
    git-svn-id: https://shooting-cubes.googlecode.com/svn@191 93678846-683e-0410-93c7-9b11d4a5ef47

--- irrlicht/source/Irrlicht/os.cpp
+++ irrlicht/source/Irrlicht/os.cpp
@@ -28,8 +28,6 @@
 		#include <sys/endian.h>
 		#define bswap_16(X) bswap16(X)
 		#define bswap_32(X) bswap32(X)
-	#elif !defined(_IRR_SOLARIS_PLATFORM_) && !defined(__PPC__)
-		#include <byteswap.h>
 	#else
 		#define bswap_16(X) ((((X)&0xFF) << 8) | (((X)&=0xFF00) >> 8))
 		#define bswap_32(X) ( (((X)&0x000000FF)<<24) | (((X)&0xFF000000) >> 24) | (((X)&0x0000FF00) << 8) | (((X) &0x00FF0000) >> 8))

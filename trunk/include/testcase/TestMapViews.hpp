a4503f14d81fba78988bd94ca36a3ad7fe1da51a
godfat <godfat@93678846-683e-0410-93c7-9b11d4a5ef47>
2008-03-17 08:25:34 +0000

    r2081@Macintosh:  godfat | 2008-03-17 16:25:14 +0800  * updated ViewSprite! not works properly, but works 50%  * we have to update data from model to view! due to cube preview,    prevent view to get the real x, y in init... and perhaps we have to    hide preview cube from visible.  * now TestMapViews create two views, one ViewSprite, one ViewStdout.  * ViewStdout is updated to redraw in 1/10 second, not all frames,    reducing lagging.
    git-svn-id: https://shooting-cubes.googlecode.com/svn@193 93678846-683e-0410-93c7-9b11d4a5ef47

--- trunk/include/testcase/TestMapViews.hpp
+++ trunk/include/testcase/TestMapViews.hpp
@@ -6,21 +6,44 @@
 #include "view/Sprite.hpp"
 #include "all_fwd.hpp"
 
+#include "presenter/Map.hpp"
+#include "data/MapSetting.hpp"
+#include "presenter/cube/ViewSprite.hpp"
+#include "presenter/cube/ViewStdout.hpp"
+
+#include <tr1/functional>
+
+using namespace psc;
+
 class TestMapViews{
 public:
     TestMapViews(){
         scene_ = psc::view::Scene::create("TestMapViewScene");
         scene_->setTo2DView();
-        
-        test_ = psc::view::Sprite::create("title", scene_, 100, 40);
-        test_->moveTo(100,40);
+
+        map_ = presenter::Map::create(data::pMapSetting(new data::MapSetting));
+
+        map_->push_view( std::make_pair(
+            &presenter::cube::ViewStdout::create,
+            &presenter::cube::ViewStdout::redraw) );
+
+        using namespace std::tr1::placeholders;
+        map_->push_view( std::make_pair(
+            std::tr1::bind(&presenter::cube::ViewSprite::create,
+                _1, _2, _3, scene_, map_->setting()),
+            &presenter::cube::ViewSprite::redraw) );
+
+        // test_ = psc::view::Sprite::create("title", scene_, 100, 40);
+        // test_->moveTo(100,40);
     }
     void cycle(){
         scene_->redraw();
+        map_->redraw().cycle();
     }
+
 private:
-    psc::view::pScene scene_;
-    psc::view::pSprite test_;
+    view::pScene scene_;
+    presenter::pMap map_;
 };
 
 #endif

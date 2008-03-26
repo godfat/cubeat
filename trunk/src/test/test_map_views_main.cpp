2a2db42da77e6ba3bf0ca2719265c87d2075eb48
godfat <godfat@93678846-683e-0410-93c7-9b11d4a5ef47>
2008-03-26 15:49:29 +0000

    r2176@220-135-28-18:  godfat | 2008-03-26 23:49:16 +0800  * moved testing stuffs to src/test  * removed empty include/test dir.
    git-svn-id: https://shooting-cubes.googlecode.com/svn@221 93678846-683e-0410-93c7-9b11d4a5ef47

--- trunk/src/test/test_map_views_main.cpp
+++ trunk/src/test/test_map_views_main.cpp
@@ -1,5 +1,55 @@
 
-#include "testcase/TestMapViews.hpp"
+#include "view/Scene.hpp"
+#include "view/Sprite.hpp"
+
+#include "presenter/Map.hpp"
+#include "presenter/cube/ViewSprite.hpp"
+#include "presenter/cube/ViewStdout.hpp"
+
+using namespace psc;
+
+class TestMapViews{
+public:
+    TestMapViews(){
+        scene_ = psc::view::Scene::create("TestMapViewScene");
+        scene_->setTo2DView();
+
+        data::pViewSpriteSetting s;
+
+        // setup map0
+        map0_ = presenter::Map::create();
+        s = data::ViewSpriteSetting::create(50, 500, 50);
+        s->push_ally(1).push_enemy(0);
+        map0_->push_view( presenter::cube::ViewSpriteMaster::create(scene_, s) );
+
+        map0_->push_view( presenter::cube::ViewStdoutMaster::create() );
+        // map0_->push_view( presenter::cube::ViewSpriteMaster::create(scene_) );
+
+        // setup map1
+        map1_ = presenter::Map::create();
+        s = data::ViewSpriteSetting::create(450, 500, 50);
+        s->push_ally(0).push_enemy(1);
+        map1_->push_view( presenter::cube::ViewSpriteMaster::create(scene_, s) );
+
+        // mini view
+        map1_->push_view( presenter::cube::ViewSpriteMaster::create(scene_,
+            data::ViewSpriteSetting::create(800, 300, 25) ) );
+
+        // setup garbage land
+        map0_->push_garbage_land(map1_);
+        map1_->push_garbage_land(map0_);
+    }
+    void cycle(){
+        scene_->redraw();
+        map0_->redraw().cycle();
+        map1_->redraw().cycle();
+    }
+
+private:
+    view::pScene scene_;
+    presenter::pMap map0_;
+    presenter::pMap map1_;
+};
 
 #include "App.hpp"
 #include <cstdlib> // for srand

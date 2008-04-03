66c3303604211630da1d4ccb728c25c875cb330d
godfat <godfat@93678846-683e-0410-93c7-9b11d4a5ef47>
2008-04-03 13:03:43 +0000

    r2230@Macintosh:  godfat | 2008-04-03 20:08:14 +0800  * updated for_each_ignore_null for vector_2d.
    git-svn-id: https://shooting-cubes.googlecode.com/svn@252 93678846-683e-0410-93c7-9b11d4a5ef47

--- trunk/include/utils/for_each_multi_array.hpp
+++ trunk/include/utils/for_each_multi_array.hpp
@@ -2,6 +2,8 @@
 #ifndef _FOR_EACH_MULTI_ARRAY_HPP_
 #define _FOR_EACH_MULTI_ARRAY_HPP_
 
+#include "utils/vector_2d.hpp"
+
 namespace psc{ namespace utils{
 
 template <class MultiArray, class Fun>
@@ -14,10 +16,10 @@ MultiArray& for_each(MultiArray& array, Fun fun){
 
 template <class MultiArray, class Fun>
 MultiArray& for_each_ignore_null(MultiArray& array, Fun fun){
-    typename MultiArray::element* data = array.origin();
-    for(int i=0, iend=array.num_elements(); i!=iend; ++i)
-        if(data[i])
-            fun(data[i], i);
+    for(typename MultiArray::size_type y=0; y != array.shape()[1]; ++y)
+        for(typename MultiArray::size_type x=0; x != array.shape()[0]; ++x)
+            if(array[x][y])
+                fun(array[x][y], x, y);
     return array;
 }
 

b77b02620293c4f0c8d41caf3250624c4fe1cd59
archilifelin@yahoo.com.tw <archilifelin@yahoo.com.tw@93678846-683e-0410-93c7-9b11d4a5ef47>
2008-04-15 11:22:14 +0000

    * starting to write SFX(special effects)... its mainly about particle system,    will be very dirty for now because I only have 1 day to write it.
    git-svn-id: https://shooting-cubes.googlecode.com/svn@321 93678846-683e-0410-93c7-9b11d4a5ef47

--- trunk/include/view/SFX.hpp
+++ trunk/include/view/SFX.hpp
@@ -3,9 +3,11 @@
 #define _SHOOTING_CUBES_SPECIAL_EFFECTS_VIEW_
 
 /* TODO:
-   Hell lots of things.
-   Have to design what kind of visual effects will occur in game,
-   and under what circumstances?      */
+   I am going to do some dirty work to get Special Effects (particles)
+   working. so the code here will not be so extensible and flexible,
+   and they will mainly depend on irrlicht's particle system without wrapper.
+
+   This class will be a singleton, for now and for the ease of use. */
 
 namespace psc { namespace view {
 

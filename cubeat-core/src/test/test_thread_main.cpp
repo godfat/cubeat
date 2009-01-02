d9d75f2022443cbf69a93e7e3e964e26375ee814
arch.jslin <archilifelin@yahoo.com.tw>
2009-01-03 04:26:58 +0800

    The thread test unit now uses real AIPlayer for testing. Looks fine for now.

--- cubeat-core/src/test/test_thread_main.cpp
+++ cubeat-core/src/test/test_thread_main.cpp
@@ -1,12 +1,13 @@
 
 #include "presenter/Stage.hpp"
+#include "ctrl/AIPlayer.hpp"
+#include "Input.hpp"
 #include "EventDispatcher.hpp"
 #include "utils/Random.hpp"
 #include "utils/dictionary.hpp"
 
 #include <tr1/functional> // for bind
 #include <boost/thread/thread.hpp>
-#include <boost/date_time.hpp>
 
 using namespace psc;
 
@@ -27,77 +28,23 @@ public:
         ctrl::EventDispatcher::i().subscribe_timer(
             std::tr1::bind(&presenter::Stage::hitGroup, stage_.get(), 2), 6000, -1);
 
-//  setup data and flag:
-        data_needed_to_be_calculated_ = 0;
-        is_calculating_ = false;
+        ctrl::Input* cpu_input = ctrl::InputMgr::i().getInputByIndex(1);
+        cpu_input->setControlledByAI(true);
+        cpu1_ = ctrl::AIPlayer::create(cpu_input);
 
-        let_us_calculate();
-
-//  persistently probing data:
-        ctrl::EventDispatcher::i().subscribe_timer(
-            std::tr1::bind(&TestThread::probe_data, this), 500, -1);
-    }
-
-    void let_us_calculate() {
-//  using thread:
-        std::cout << "start to calculate...." << std::endl;
-        ctrl::EventDispatcher::i().subscribe_timer(
-            std::tr1::bind(&TestThread::start_thread, this), 2000);
-
-//  not using thread:
-//        ctrl::EventDispatcher::i().subscribe_timer(
-//            std::tr1::bind(&TestThread::do_thread_work, this), 2000);
-    }
-
-    void start_thread() {
-        if( !is_calculating_ ) {
-            pthread_ = boost::shared_ptr<boost::thread>(
-                new boost::thread(std::tr1::bind(&TestThread::do_thread_work, this)));
-        } else { //if the calculation isn't done yet, wait for a little more time
-            ctrl::EventDispatcher::i().subscribe_timer(
-                std::tr1::bind(&TestThread::start_thread, this), 1000);
-        }
-    }
-
-    void do_thread_work() {
-        //boost::mutex::scoped_lock lk(mutex_);
-        is_calculating_ = true;
-        std::cout << "Hi!." << std::endl;
-        for( volatile int i = 0; i < 200000000; i++ ) {
-            data_needed_to_be_calculated_ += 1;
-            if( i % 20000000 == 0 ) std::cout << "calculating..." << std::endl;
-        }
-        //boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
-        std::cout << "done calculating." << std::endl;
-        is_calculating_ = false;
-        if( data_needed_to_be_calculated_ < 1000000000 )
-            let_us_calculate();
-        else
-            stop_calculating();
-    }
-
-    void stop_calculating() {
-        if( pthread_ ) {
-            std::cout << "The calculating is done, thread joined." << std::endl;
-            pthread_->join();
-        }
-    }
-
-    void probe_data() {
-        std::cout << "report data: " << data_needed_to_be_calculated_ << std::endl;
+        if( !cpu1_->startThinking() )
+            std::cout << "CPU AI failed the initialization." << std::endl;
     }
 
     void cycle(){
         stage_->cycle();
+        //cpu1_->cycle();
     }
 
 private:
-    int data_needed_to_be_calculated_;
-    bool is_calculating_;
-
     boost::mutex  mutex_;
-    boost::shared_ptr<boost::thread> pthread_;
     presenter::pStage stage_;
+    ctrl::pAIPlayer cpu1_;
 };
 
 #include "App.hpp"

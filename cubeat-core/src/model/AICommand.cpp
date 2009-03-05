84a0a6a3d0163422048b6aed89d27411f03d258e
arch.jslin <archilifelin@yahoo.com.tw>
2009-03-05 09:04:54 +0800

    minor code clean up in AICommand.cpp

--- cubeat-core/src/model/AICommand.cpp
+++ cubeat-core/src/model/AICommand.cpp
@@ -25,17 +25,17 @@ AICommand& AICommand::release(BtnID::BID btn)
 }
 
 //shortcuts
-AICommand& AICommand::press_haste() { press(BtnID::HASTE); return *this; }
-AICommand& AICommand::press_trig1() { press(BtnID::TRIG_1); return *this; }
-AICommand& AICommand::press_trig2() { press(BtnID::TRIG_2); return *this; }
-AICommand& AICommand::press_wep1() { press(BtnID::WEP_1); return *this; }
-AICommand& AICommand::press_wep2() { press(BtnID::WEP_2); return *this; }
-AICommand& AICommand::press_wep3() { press(BtnID::WEP_3); return *this; }
-AICommand& AICommand::release_haste() { release(BtnID::HASTE); return *this; }
-AICommand& AICommand::release_trig1() { release(BtnID::TRIG_1); return *this; }
-AICommand& AICommand::release_trig2() { release(BtnID::TRIG_2); return *this; }
-AICommand& AICommand::release_wep1() { release(BtnID::WEP_1); return *this; }
-AICommand& AICommand::release_wep2() { release(BtnID::WEP_2); return *this; }
-AICommand& AICommand::release_wep3() { release(BtnID::WEP_3); return *this; }
-AICommand& AICommand::normal_shot(int x, int y) { move_to(x, y).press_trig1(); return *this; }
-AICommand& AICommand::weapon_shot(int x, int y) { move_to(x, y).press_trig2(); return *this; }
+AICommand& AICommand::press_haste() { return press(BtnID::HASTE); }
+AICommand& AICommand::press_trig1() { return press(BtnID::TRIG_1); }
+AICommand& AICommand::press_trig2() { return press(BtnID::TRIG_2); }
+AICommand& AICommand::press_wep1() { return press(BtnID::WEP_1); }
+AICommand& AICommand::press_wep2() { return press(BtnID::WEP_2); }
+AICommand& AICommand::press_wep3() { return press(BtnID::WEP_3); }
+AICommand& AICommand::release_haste() { return release(BtnID::HASTE); }
+AICommand& AICommand::release_trig1() { return release(BtnID::TRIG_1); }
+AICommand& AICommand::release_trig2() { return release(BtnID::TRIG_2); }
+AICommand& AICommand::release_wep1() { return release(BtnID::WEP_1); }
+AICommand& AICommand::release_wep2() { return release(BtnID::WEP_2); }
+AICommand& AICommand::release_wep3() { return release(BtnID::WEP_3); }
+AICommand& AICommand::normal_shot(int x, int y) { return move_to(x, y).press_trig1(); }
+AICommand& AICommand::weapon_shot(int x, int y) { return move_to(x, y).press_trig2(); }

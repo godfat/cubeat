4f906029f3adf94a0234189b3478216e25f4032b
Archilife Lin <archilifelin@yahoo.com.tw>
2010-06-26 00:33:31 +0800

    I am such a fool to link against the old lib file and include the old header files. simply updated project file to point them to the right path and everything solved. Picking in Irrlicht 1.7.1 seems to be fine, however the OpenGL alpha transparent material is not working in the way I want. Some additional investigation is needed.

--- cubeat-core/src/test/test_irr_device_main.cpp
+++ cubeat-core/src/test/test_irr_device_main.cpp
@@ -1,4 +1,5 @@
 /** Example 001 HelloWorld From Official Irrlicht Example **/
+#include <iostream>
 #include <irrlicht.h>
 
 using namespace irr;
@@ -44,7 +45,7 @@ int main()
 	dimensions, etc.
 	*/
 	IrrlichtDevice *device =
-		createDevice( video::EDT_SOFTWARE, dimension2d<u32>(640, 480), 16,
+		createDevice( video::EDT_OPENGL, dimension2d<u32>(640, 480), 16,
 			false, false, false, 0);
 
 	if (!device)
@@ -52,12 +53,12 @@ int main()
 
 	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
 	IVideoDriver* driver = device->getVideoDriver();
-/*	ISceneManager* smgr = device->getSceneManager();
+	ISceneManager* smgr = device->getSceneManager();
 	IGUIEnvironment* guienv = device->getGUIEnvironment();
 	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
-		rect<s32>(10,10,260,22), true); */
-/*
-	IAnimatedMesh* mesh = smgr->getMesh("../../rc/model/test_cube.x");
+		rect<s32>(10,10,260,22), true);
+
+	IAnimatedMesh* mesh = smgr->getMesh("rc/model/test_cube.x");
 	if (!mesh)
 	{
 		device->drop();
@@ -68,14 +69,14 @@ int main()
 	if (node) {
 		node->setMaterialFlag(EMF_LIGHTING, false);
 		node->setMD2Animation(scene::EMAT_STAND);
-		node->setMaterialTexture( 0, driver->getTexture("../../rc/texture/circle.png") );
-	} */
-//	smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
+		node->setMaterialTexture( 0, driver->getTexture("rc/texture/circle.png") );
+	}
+	smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
 
 	while(device->run()) {
 		driver->beginScene(true, true, SColor(255,100,101,140));
-//		smgr->drawAll();
-//		guienv->drawAll();
+		smgr->drawAll();
+		guienv->drawAll();
 		driver->endScene();
 	}
 	device->drop();

fd4d1b10066a78462f4fad26ed3fd3c0250eb7ce
Johnson Lin <arch.jslin@gmail.com>
2011-04-06 22:10:27 +0800

    Waht the fuck?

--- cubeat-core/src/presenter/game/Multi.cpp
+++ cubeat-core/src/presenter/game/Multi.cpp
@@ -330,7 +330,6 @@ void Multi::setup_end_button()
 
 void Multi::end_sequence1()
 {
-    std::cout << "Waht the fuck.\n";
     Sound::i().play("4/4c.wav");
     btn_reinit_.reset();
     stage_->releaseResource(); //release when player isn't going to replay

89617bb54b8dc4a395d06f2742c6d02fdcbf8fb5
Johnson Lin <arch.jslin@gmail.com>
2011-04-10 03:21:54 +0800

    puzzle: removed old gen_puzzle script for win32. utils::MapLoader cleaned up a bit as well.

--- cubeat-core/bin/mac_workaround_for_irrklang.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-install_name_tool -change /usr/local/lib/libirrklang.dylib libirrklang.dylib bin/libcubeat.dylib
-
-install_name_tool -change /usr/local/lib/libirrklang.dylib libirrklang.dylib bin/cubeat

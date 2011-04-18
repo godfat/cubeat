aeff478b893548d62488da44a579d72236d9cc0e
Johnson Lin <arch.jslin@gmail.com>
2011-04-18 23:52:08 +0800

    IP issue: copy&paste the same copyright notice from header to source as well.

--- cubeat-core/src/private/MastEventReceiver.cpp
+++ cubeat-core/src/private/MastEventReceiver.cpp
@@ -1,4 +1,25 @@
 
+/// ==================================================================================================
+/// MastEventReceiver code is © (Copyright) Robert E. Demarest, AKA Mastiff or Mastiff Odit
+/// This file may be used in any non-commercial or commercial project as long as the following conditions are met:
+/// You may not claim this code as being your own.
+/// You may not use this code for any harmful, malicious or otherwise damaging programs.
+///
+/// This is version 1.2a of the class.
+/// This class is designed for use with the Irrlicht Engine, it was written for version 1.3 of the engine.
+/// ==================================================================================================
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// To use this Class just add  #include "MastEventReceiver.cpp"  to the end of your includes list. (or add the class in-line into your program)
+// Then create an instance of it like so: MastEventReceiver eventReceiver;
+// Then call the initialization fucntion like so: eventReceiver.init();
+// Then inside your Main Game Loop place "eventReceiver.endEventProcess();" in the beginning of your game loop, before anything -
+// that would require input, then put "eventReceiver.startEventProcess();" at the very end of your Main Game Loop.
+// yeah I know it's confusing, but it makes alot more sense in the internals of the class.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #include "private/MastEventReceiver.hpp"
 
 using namespace irr;

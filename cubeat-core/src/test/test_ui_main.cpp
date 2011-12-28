b119ccd19da6c75704195bba3d1b8913e2b133f9
Johnson Lin <arch.jslin@gmail.com>
2011-12-28 17:50:19 +0800

    forgot to add the file...

---
+++ cubeat-core/src/test/test_ui_main.cpp
@@ -0,0 +1,49 @@
+
+#include "presenter/Stage.hpp"
+#include "EventDispatcher.hpp"
+#include "utils/Random.hpp"
+#include "utils/dictionary.hpp"
+#include "Conf.hpp"
+
+using namespace psc;
+
+class TestStage{
+    typedef std::vector< view::pAnimatedSceneObject > SceneObjList;
+
+public:
+    TestStage(){
+        //temporary Scene:
+
+        utils::map_any stage = Conf::i().config_of("test_stage");
+        stage_ = presenter::Stage::create( stage.S("test_stage") );
+        stage_->playBGM();
+
+        ctrl::EventDispatcher::i().get_timer_dispatcher("global")->subscribe(
+            std::tr1::bind(&presenter::Stage::hitGroup, stage_.get(), 1), 3000, -1);
+
+        ctrl::EventDispatcher::i().get_timer_dispatcher("global")->subscribe(
+            std::tr1::bind(&presenter::Stage::hitGroup, stage_.get(), 2), 6000, -1);
+    }
+
+    void cycle(){
+        stage_->cycle();
+    }
+
+private:
+    presenter::pStage stage_;
+};
+
+#include "App.hpp"
+#include <cstdlib> // for srand
+#include <ctime> // for time, clock
+
+#include <boost/tr1/functional.hpp> // for bind
+
+int main(){
+    std::srand(std::time(0)^std::clock()); //  init srand for global rand...
+    psc::Conf::i().init("");
+    psc::App::i();
+    TestStage tester;
+    return psc::App::i().run(std::tr1::bind(&TestStage::cycle, &tester));
+}
+

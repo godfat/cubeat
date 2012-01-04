8130d1827e4c021aae917733e460feb42cd03ce1
Johnson Lin <arch.jslin@gmail.com>
2012-01-04 16:14:52 +0800

    cleaned up the code a bit.

--- cubeat-core/rc/script/ui/test/view.lua
+++ cubeat-core/rc/script/ui/test/view.lua
@@ -11,6 +11,25 @@ typedef struct pSprite pSprite;
 ]]
 ffi.cdef( io.open( basepath().."rc/script/ui/test/bindings.ffi", 'r'):read('*a') )
 
+------------- Some utils rigged for ffi callback management ---------------
+
+local function tracked_cb(cb_table, o, func)
+  if cb_table[o] == nil then
+    cb_table[o] = ffi.cast("PSC_OBJCALLBACK", func)
+  else
+    cb_table[o]:set(func)
+  end
+  return cb_table[o]
+end
+
+local function tracked_cb_removal(cb_table, o)
+  if cb_table[o] then 
+    cb_table[o]:free(); cb_table[o] = nil
+  end
+end
+
+------------- "Class" definitions -----------------------------------------
+
 local Mt_TestUI = {}
 Mt_TestUI.__index      = Mt_TestUI
 
@@ -36,28 +55,17 @@ Mt_Sprite.__on_downs__    = setmetatable({}, weakkey)
 Mt_Sprite.__on_ups__      = setmetatable({}, weakkey)
 
 Mt_Sprite.on_release = function(self, func)
-  if Mt_Sprite.__on_releases__[self] == nil then
-    Mt_Sprite.__on_releases__[self] = ffi.cast("PSC_OBJCALLBACK", func)
-  else
-    Mt_Sprite.__on_releases__[self]:set(func)
-  end
-  C.Sprite_on_release(self, Mt_Sprite.__on_releases__[self])
+  C.Sprite_on_release(self, tracked_cb(Mt_Sprite.__on_releases__, self, func))
 end
 
 ffi.metatype("pSprite", Mt_Sprite)
 
-local function event_removal(cb_table, o)
-  if cb_table[o] then 
-    cb_table[o]:free(); cb_table[o] = nil
-  end
-end
-
 local function newSprite(name, scene, w, h, center)
   return ffi.gc(C.Sprite_create(name, scene, w, h, center), function(self)
-    event_removal(Mt_Sprite.__on_releases__, self)
-    event_removal(Mt_Sprite.__on_presses__, self)
-    event_removal(Mt_Sprite.__on_downs__, self)
-    event_removal(Mt_Sprite.__on_ups__, self)
+    tracked_cb_removal (Mt_Sprite.__on_releases__, self)
+    tracked_cb_removal (Mt_Sprite.__on_presses__, self)
+    tracked_cb_removal (Mt_Sprite.__on_downs__, self)
+    tracked_cb_removal (Mt_Sprite.__on_ups__, self)
     C.Sprite__gc(self)
   end)
 end

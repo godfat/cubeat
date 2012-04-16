4302e48ad21fc5c31c7d82e8dafdef1fb2ef7e0e
Johnson Lin <arch.jslin@gmail.com>
2012-04-16 17:21:50 +0800

    still have to add a net::Manager first. otherwise it will be too messy

---
+++ cubeat-core/include/net/Manager.hpp
@@ -0,0 +1,14 @@
+#ifndef _SHOOTING_CUBES_NET_MANAGER_
+#define _SHOOTING_CUBES_NET_MANAGER_
+
+namespace psc {
+namespace net {
+
+class Manager{};
+
+} //net
+} //psc
+
+#endif //_SHOOTING_CUBES_NET_MANAGER_
+
+
---
+++ cubeat-core/src/net/Manager.cpp
@@ -0,0 +1,2 @@
+
+#include "net/Manager.hpp"

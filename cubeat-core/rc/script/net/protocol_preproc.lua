496bf3c1be89b229ce24aa9c8989ba2f35f329d8
Johnson Lin <arch.jslin@gmail.com>
2012-04-29 14:40:30 +0800

    will this is just follow up psc_net_test/master

--- cubeat-core/rc/script/net/protocol_preproc.lua
+++ cubeat-core/rc/script/net/protocol_preproc.lua
@@ -11,24 +11,24 @@ local EXPORT = {}
 local net    = nil
 local game   = nil
 
-
--- recv functions : connection receiver
+-- recv functions
 local RECV = {}
 
 RECV.URE = function(m)
   dump(m.T)
-  game.pid = m.pid
 
+  game.pid = m.pid
   table.foreach(m.ppl, function(k, v) v.addr = kit.addr_ext(v.addr) end)
   game.ppl = m.ppl
 
-  net.gotoLobbyReady() -- switch net state
+  net.gotoLobbyReady()
 
   EXPORT.plist(m.src)  -- test
 end
 
 RECV.GREETING = function(m)
   pmsg(m)
+
   net.gotGreeting(m.src)
 end
 
@@ -37,6 +37,10 @@ RECV.PLS_R = function(m)
   if m.C==0 then
     table.foreach(m.ppl, function(k, v) v.addr = kit.addr_ext(v.addr) end)
     game.ppl = m.ppl
+    game.ppl_map = {}
+    table.foreach(game.ppl, function(k, v)
+      game.ppl_map[v.pid] = v
+    end)
   end
 end
 
@@ -49,6 +53,19 @@ RECV.CHAT = function(m)
     dump(sid..' says '..txt..' at Lobby')
   end
 end
+
+RECV.PLAY_W = function(m)
+  pmsg(m)
+  if m.C == 0 then
+    m.tar.addr = kit.addr_ext(m.tar.addr)
+    game.ppl_map[m.tar.pid] = m.tar
+
+    net.gotoPlayer(m.tar)
+  else
+    dump('no such player: '..m.tar.pid)
+  end
+end
+
 
 -- receiver
 local recv = kit.getRecv(function (m)
@@ -65,33 +82,41 @@ local function greeting(peer)
   local m = msg('GREETING')
   kit.send(m, peer)
 end
-local function send_iam(ip, port, peer)
+local function send_iam(ip, port, pserv)
   local m = msg('IAM')
   m.ip    = ip
   m.port  = port
   m.nick  = 'nick '..string.random(4, '%d')
-  kit.send(m, peer)
+  kit.send(m, pserv)
 end
-local function plist(peer)
+local function plist(pserv)
   local m = msg('PLS')
   m.pid   = game.pid
-  kit.send(m, peer)
+  kit.send(m, pserv)
 end
-local function poke_server(peer)
+local function poke_server(pserv)
   local m = msg('POKE')
   m.pid   = game.pid
-  kit.send(m, peer)
+  kit.send(m, pserv)
 end
-local function chat_lobby(peer, txt)
+local function chat_lobby(pserv, txt)
   local m = msg('CHAT')
   m.pid   = game.pid
   m.txt   = txt
   m.type  = 'b' -- lobby
-  kit.send(m, peer)
+  kit.send(m, pserv)
+end
+local function play_one(pserv, pid) -- tell server who I want to play with
+  local m = msg('PLAY_1')
+  m.pid_me  = game.pid
+  m.pid_tar = pid
+  kit.send(m, pserv)
 end
 
+EXPORT.on = function(k,f) ON[k] = f end
 EXPORT.chat_lobby  = chat_lobby
 EXPORT.poke_server = poke_server
+EXPORT.play_one = play_one
 EXPORT.send_iam = send_iam
 EXPORT.greeting = greeting
 EXPORT.recv  = recv

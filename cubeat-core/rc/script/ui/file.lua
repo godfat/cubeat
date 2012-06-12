a8a1081c8bfb16114d4039bf2f0ff59c17040fc5
Maxwell <maxwellrocker@gmail.com>
2012-06-12 14:27:53 +0800

    rewrite all file.lua

--- cubeat-core/rc/script/ui/file.lua
+++ cubeat-core/rc/script/ui/file.lua
@@ -2,28 +2,45 @@ local msgpack   = require 'rc/script/ui/luajit-msgpack-pure'
 local helper    = require 'rc/script/helper'
 local basepath  = helper.basepath
 
-local function load_data(filename, data)
-  if filename == nil then error('load_data error -- the filename is nil') end
-  if data     == nil then data={} end
+local function check_filename(filename)
+  if filename==nil then
+    error('error -- the filename is nil') end
+  if string.len(filename)<1 then
+    error('error -- the filename length is 0') end
+end
+local function check_data(data)
+  if data==nil then
+    error('error -- the data is nil') end
+end
+
+local function load_data(filename)
+  check_filename(filename)
   
+  local offset
+  local data
   local file = io.open(basepath().."rc/config/"..filename, "r")
   if file == nil then
-    io.output(basepath().."rc/config/"..filename)
-    io.close()
+    print('Could not open this file: '..filename)
   else
-    local offset
     offset, data = msgpack.unpack( file:read() )
+    if data=="invalid argument" then data=nil end
     file:close()
   end
+
   return data
 end
 
 local function save_data(filename, data)
-  if filename == nil then error('save_data error -- the filename is nil') end
-  if data     == nil then data={} end
+  check_filename(filename)
+  check_data(data)
   
   local s = msgpack.pack(data)
   local file = io.open(basepath().."rc/config/"..filename, "w")
+  if file==nil then
+    io.output(basepath().."rc/config/"..filename)
+    io.close()
+    file = io.open(basepath().."rc/config/"..filename, "w")
+  end
   file:write(s)
   file:close()
 end

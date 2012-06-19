32e44ff483c1b48018d14435e6313bee2de55e7f
Maxwell <maxwellrocker@gmail.com>
2012-06-19 14:05:01 +0800

    add tween_call() function - it will call correspond template function

---
+++ cubeat-core/include/script/ui/tween_call.h
@@ -0,0 +1,16 @@
+#ifndef _TWEEN_CALL_H_
+#define _TWEEN_CALL_H_
+
+#include <boost/tr1/functional.hpp>
+#include "view/Sprite.hpp"
+
+using namespace psc;
+using namespace view;
+
+typedef struct { int x; }           value1;
+typedef struct { double x, y; }     value2;
+typedef struct { double x, y, z; }  value3;
+
+void tween_call(pSprite*, const char*, const char*, void*, void*, unsigned int, int, std::tr1::function<void()>, int);
+
+#endif

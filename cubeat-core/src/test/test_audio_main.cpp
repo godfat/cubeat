1ca7531756011a78876a1cf59f7e6405fd73a546
Johnson Lin <arch.jslin@gmail.com>
2012-07-25 16:43:46 +0800

    update audio::Sound usage according to fixed interface. added a dedicated test case for audio as well.

--- cubeat-core/src/test/test_audio_main.cpp
+++ cubeat-core/src/test/test_audio_main.cpp
@@ -19,38 +19,53 @@ public:
         utils::map_any stage = Conf::i().config_of("test_stage");
         stage_ = presenter::Stage::create( stage.S("test_stage") );
         //stage_->playBGM();
-        audio::Sound::i().playBGM("jungle1/bgm_b.ogg");
+        audio::Sound::i().playBGM_AB("jungle4/bgm_a.ogg", "jungle4/bgm_b.ogg");
+
+        int play_time = 10000;
 
         ctrl::EventDispatcher::i().get_timer_dispatcher("global")->subscribe(
-            std::tr1::bind(&TestAudio::playBGM_delayed, this, 2), 3000);
+            std::tr1::bind(&TestAudio::playBGM_delayed, this, "smb_warning.wav"), play_time);
+
+        play_time += 2500;
 
         ctrl::EventDispatcher::i().get_timer_dispatcher("global")->subscribe(
-            std::tr1::bind(&TestAudio::playBGM_delayed, this, 3), 6000);
+            std::tr1::bind(&TestAudio::playBGM_AB_delayed, this, "jungle4/bgm_quick_a.ogg", "jungle4/bgm_quick_b.ogg"), play_time);
+
+        play_time += 10000;
 
         ctrl::EventDispatcher::i().get_timer_dispatcher("global")->subscribe(
-            std::tr1::bind(&TestAudio::playBGM_delayed, this, 4), 9000);
+            std::tr1::bind(&TestAudio::playBGM_AB_delayed, this, "jungle4/bgm_a.ogg", "jungle4/bgm_b.ogg"), play_time);
+
+        play_time += 10000;
 
         ctrl::EventDispatcher::i().get_timer_dispatcher("global")->subscribe(
-            std::tr1::bind(&TestAudio::playBGM_delayed, this, 5), 12000);
+            std::tr1::bind(&TestAudio::playBGM_delayed, this, "smb_warning.wav"), play_time);
+
+        play_time += 2500;
 
         ctrl::EventDispatcher::i().get_timer_dispatcher("global")->subscribe(
-            std::tr1::bind(&TestAudio::playBGM_delayed, this, 6), 15000);
+            std::tr1::bind(&TestAudio::playBGM_AB_delayed, this, "jungle4/bgm_quick_a.ogg", "jungle4/bgm_quick_b.ogg"), play_time);
 
         ctrl::EventDispatcher::i().get_timer_dispatcher("global")->subscribe(
             std::tr1::bind(&presenter::Stage::hitGroup, stage_.get(), 1), 3000, -1);
 
         ctrl::EventDispatcher::i().get_timer_dispatcher("global")->subscribe(
             std::tr1::bind(&presenter::Stage::hitGroup, stage_.get(), 2), 6000, -1);
+
+        ctrl::EventDispatcher::i().get_timer_dispatcher("global")->subscribe(
+            std::tr1::bind(&audio::Sound::check_sound_volumes, &audio::Sound::i()), 1000, -1);
     }
 
     void cycle(){
         stage_->cycle();
     }
 
-    void playBGM_delayed(int number){
-        std::ostringstream oss;
-        oss << "jungle" << number << "/bgm_b.ogg";
-        audio::Sound::i().playBGM(oss.str());
+    void playBGM_delayed(std::string const& path){
+        audio::Sound::i().playBGM(path, 400);
+    }
+
+    void playBGM_AB_delayed(std::string const& path_a, std::string const& path_b){
+        audio::Sound::i().playBGM_AB(path_a, path_b, 400);
     }
 
 private:

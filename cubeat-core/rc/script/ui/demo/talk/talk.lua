cdbcd6ba0a963f21c5906529b19c052c67eded4f
Maxwell <maxwellrocker@gmail.com>
2012-08-01 16:11:24 +0800

    adjust talk page create code in talk.lua line19~29

--- cubeat-core/rc/script/ui/demo/talk/talk.lua
+++ cubeat-core/rc/script/ui/demo/talk/talk.lua
@@ -1,27 +1,32 @@
-local ffi   = require 'ffi'
-local C     = ffi.C
-local view  = require 'rc/script/ui/view'
-local ui    = require 'rc/script/ui/ui'
-local config= require 'rc/script/ui/demo/talk/config'
-local switch= require 'rc/script/ui/demo/switch/switch'
+local ffi     = require 'ffi'
+local C       = ffi.C
+local view    = require 'rc/script/ui/view'
+local ui      = require 'rc/script/ui/ui'
+local config  = require 'rc/script/ui/demo/talk/config'
+local switch  = require 'rc/script/ui/demo/switch/switch'
+local select_config = require 'rc/script/ui/demo/select/config'
 
 local function init(demo, parent)
   local menu = {}
   
+  local ch_choose = {}
+  ch_choose[1] = select_config.ch_choose[1]
+  ch_choose[2] = select_config.ch_choose[2]
+  
   menu.TalkBackGround = ui.new_image{ parent=parent, path=config.bg_path, x=config.bg_x, y=config.bg_y,
                                       w=config.bg_w, h=config.bg_h }
-  menu.TalkActorL     = ui.new_image{ parent=parent, path='char1_new/glad', x=config.act_L_x, y=config.act_L_y,
-                                      w=config.act_w, h=config.act_h, depth=config.act_d }
-  menu.TalkActorR     = ui.new_image{ parent=parent, path='char2_new/glad', x=config.act_R_x, y=config.act_R_y,
-                                      w=config.act_w, h=config.act_h, depth=config.act_d }
-
-  menu.ContentL = ui.new_text{ parent=parent, title='ContentR', x=config.con_L_x, y=config.con_L_y, size=32 }
-  menu.ContentR = ui.new_text{ parent=parent, title='ContentL', x=config.con_R_x, y=config.con_R_y, size=32 }
   
-  menu.ContentBG_L  = ui.new_image{ parent=parent, path='area_rect', x=config.conBG_L_x, y=config.conBG_L_y,
-                                    w=config.conBG_w, h=config.conBG_h }
-  menu.ContentBG_R  = ui.new_image{ parent=parent, path='area_rect', x=config.conBG_R_x, y=config.conBG_R_y,
-                                    w=config.conBG_w, h=config.conBG_h }
+  for i=1,2 do
+    local ch_path = 'char'..tostring(ch_choose[i])..'_new/glad'
+    local actor   = 'actor'..tostring(i)
+    local content = 'content'..tostring(i)
+    local panel   = 'panel'..tostring(i)
+    menu[actor]   = ui.new_image{ parent=parent, path=ch_path, x=config.act_x[i], y=config.act_y[i],
+                                  w=config.act_w, h=config.act_h, depth=config.act_d }
+    menu[content] = ui.new_text{ parent=parent, title=' ', x=config.con_x[i], y=config.con_y[i], size=32 }
+    menu[panel]   = ui.new_image{ parent=parent, path='area_rect', x=config.conBG_x[i], y=config.conBG_y[i],
+                                  w=config.conBG_w, h=config.conBG_h }
+  end
   
   menu.back     = ui.new_text{ parent=parent, title='Back', x=0, y=300, size=32, depth=-100 }
   menu.back:on_press(function(self)

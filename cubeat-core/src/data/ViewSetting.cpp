373295f267c3d7ae6fd70c4eed7363ffe680ea1f
Johnson Lin <arch.jslin@gmail.com>
2012-08-08 13:09:15 +0800

    ViewSetting set with utils::map_any

---
+++ cubeat-core/src/data/ViewSetting.cpp
@@ -0,0 +1,20 @@
+
+#include "data/ViewSetting.hpp"
+#include "utils/dictionary.hpp"
+#include "Conf.hpp" // we possibly need screen resolution data in the future here
+
+using namespace psc;
+using namespace data;
+
+ViewSetting::ViewSetting(utils::map_any const& conf):
+    x_offset_( conf.I("xoff") ),
+    y_offset_( conf.I("yoff") ),
+    z_offset_( 0 ),
+    cube_size_( conf.I("csize") ),
+    ats_x_( conf.I("garbage_src_x") ),
+    ats_y_( conf.I("garbage_src_y") ),
+    atf_x_( conf.I("garbage_ene_x") ),
+    atf_y_( ats_y_ )
+{
+}
+

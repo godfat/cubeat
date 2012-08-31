5d6565cd3a40f08a299632a565d936a2b936fd5b
Johnson Lin <arch.jslin@gmail.com>
2012-08-31 19:01:18 +0800

    testing stage by lua

--- cubeat-core/rc/script/test_stage.lua
+++ cubeat-core/rc/script/test_stage.lua
@@ -4,8 +4,10 @@ local C    = ffi.C
 local view = require 'rc/script/ui/view'
 
 local scene_
+local layer0_
 local layer1_
 local layer2_
+local layer22_
 local layer3_
 local layer4_
 local layer5_
@@ -16,20 +18,57 @@ local layer9_
 
 function init(scene)
   scene_ = ffi.cast("pScene*", scene)
-  layer1_ = view.new_sprite("bg1/01sky", scene_, 1280, 720, false):set_depth(-1)
-  layer2_ = view.new_sprite("bg1/02cloud1", scene_, 1280, 720, false):set_depth(-2)
-  layer3_ = view.new_sprite("bg1/03cloud2", scene_, 1280, 720, false):set_depth(-3)
-  layer4_ = view.new_sprite("bg1/04cloud3", scene_, 1280, 720, false):set_depth(-4)
-  layer5_ = view.new_sprite("bg1/05cloud4", scene_, 1280, 720, false):set_depth(-5)
-  layer6_ = view.new_sprite("bg1/06cube", scene_, 1280, 720, false):set_depth(-6)
-  layer7_ = view.new_sprite("bg1/07flyingwhale", scene_, 1280, 720, false):set_depth(-7)
-  layer8_ = view.new_sprite("bg1/09building", scene_, 1280, 720, false):set_depth(-8)
-  layer9_ = view.new_sprite("bg1/08whaleface", scene_, 1280, 720, false):set_depth(-9)
+  -- layer0_ = view.new_sprite("bg1/99complete", scene_)
+  -- layer0_:set_depth(0)
+  
+  layer1_ = view.new_sprite("bg1/01sky", scene_)       
+  layer1_:set_depth(-10)
+  layer1_:set_pos( 1280 - layer1_:get_size_x(), 0 )
+  
+  layer2_ = view.new_sprite("bg1/02cloud1", scene_)
+  layer2_:set_depth(-20)
+  layer2_:tween("Linear", "Pos2D", ffi.new("v2", 910, 245), ffi.new("v2", 1020, 245), 20000, -1)
+  layer2_:tween("SineCirc", "Alpha", 0, 255, 20000, -1)
+  
+  layer22_ = view.new_sprite("bg1/02cloud1", scene_)
+  layer22_:set_depth(-20)
+  layer22_:set_alpha(0)
+  layer22_:tween("Linear", "Pos2D", ffi.new("v2", 870, 245), ffi.new("v2", 980, 245), 20000, -1, nil, 15000)
+  layer22_:tween("SineCirc", "Alpha", 0, 255, 20000, -1, nil, 15000)
+  
+  layer3_ = view.new_sprite("bg1/03cloud2", scene_)
+  layer3_:set_depth(-30)
+  layer3_:set_pos(425, 300)
+  
+  layer4_ = view.new_sprite("bg1/04cloud3", scene_)
+  layer4_:set_depth(-40)
+  layer4_:set_pos( 1280 - layer4_:get_size_x(), 0 )
+  
+  layer5_ = view.new_sprite("bg1/05cloud4", scene_)
+  layer5_:set_depth(-50)
+  layer5_:set_pos(325, 0)
+  
+  layer6_ = view.new_sprite("bg1/06cube", scene_)
+  layer6_:set_depth(-60)
+  layer6_:set_pos(500, 107)
+  
+  layer7_ = view.new_sprite("bg1/07flyingwhale", scene_)
+  layer7_:set_depth(-70)
+  layer7_:set_pos(1078, 199)
+  layer7_:tween("SineCirc", "Pos2D", ffi.new("v2", 1070, 199), ffi.new("v2", 1090, 199), 7000, -1)
+  
+  layer8_ = view.new_sprite("bg1/09building", scene_)
+  layer8_:set_depth(-80)
+  
+  layer9_ = view.new_sprite("bg1/08whaleface", scene_)
+  layer9_:set_depth(-90)
+  layer9_:set_pos(180, 0)
 end
 
 function cleanup()
   layer1_ = nil
   layer2_ = nil
+  layer22_ = nil
   layer3_ = nil
   layer4_ = nil
   layer5_ = nil

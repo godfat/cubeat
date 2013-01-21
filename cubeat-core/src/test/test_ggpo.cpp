35869f26770e7df1d9f68191a06ac9f927d490a6
Johnson Lin <arch.jslin@gmail.com>
2013-01-21 17:51:44 +0800

    So ggpo doesn't encapsulate certain system calls. I'll have to do that. Next item on list is EventDispatcher's container should be getting along with ObjectPoolRestorable.

--- cubeat-core/src/test/test_ggpo.cpp
+++ cubeat-core/src/test/test_ggpo.cpp
@@ -7,14 +7,103 @@
 #include "utils/Random.hpp"
 #include "Conf.hpp"
 
+#include "ggponet.h"
+
+#if defined(__WIN32__) || defined(WIN32)
+#include "windows.h"
+#endif
+
 using namespace psc;
+
+bool test_ggpo_begin_game(char* name) { // do nothing?
+    return true;
+}
+
+bool test_ggpo_advance_frame(int flags) {
+    return true;
+}
+
+bool test_ggpo_load_state(unsigned char* buf, int len) {
+    return true;
+}
+
+bool test_ggpo_save_state(unsigned char** buf, int *len, int* checksum, int frame) {
+    return true;
+}
+
+void test_ggpo_free_buffer(void* buf) {
+}
+
+bool test_ggpo_net_log(char* filename, unsigned char* buffer, int len) {
+    return true;
+}
+
+bool test_ggpo_net_event(GGPOEvent* e) {
+    return true;
+}
+
+static time_t realtime()
+{
+    return IrrDevice::i().d()->getTimer()->getRealTime();
+}
 
 class TestGGPO{
 public:
-    TestGGPO(): frame_(0), rollbacked_(false) {
+    TestGGPO(): frame_(0), rollbacked_(false), ggpo_(0) {
         scene_ = view::Scene::create("game");
         scene_->setTo2DView();
         utils::Random2::i().init(12345);
+
+        ggpo_setup();
+    }
+
+    ~TestGGPO() {
+        cleanup();
+    }
+
+    void ggpo_setup() {
+#if defined(__WIN32__) || defined(WIN32)
+        WSADATA wd = { 0 };
+        WSAStartup(MAKEWORD(2, 2), &wd);
+#else
+        printf("WTF: Networking implementations are not available for this platform yet.\n");
+#endif
+        ggpo_callbacks_.begin_game      = test_ggpo_begin_game;
+        ggpo_callbacks_.advance_frame   = test_ggpo_advance_frame;
+        ggpo_callbacks_.load_game_state = test_ggpo_load_state;
+        ggpo_callbacks_.save_game_state = test_ggpo_save_state;
+        ggpo_callbacks_.free_buffer     = test_ggpo_free_buffer;
+        ggpo_callbacks_.on_event        = test_ggpo_net_event;
+
+        //necessary?
+        ggpo_callbacks_.log_game_state  = test_ggpo_net_log;
+
+        GGPOErrorCode result = ggpo_start_session(&ggpo_, &ggpo_callbacks_, "psc::TestGGPO", 2, sizeof(int), 7000);
+        printf("GGPO Error: %d\n", result);
+
+        ggpo_set_disconnect_timeout(ggpo_, 3000);
+        ggpo_set_disconnect_notify_start(ggpo_, 3000);
+
+        GGPOPlayer p1, p2;
+        GGPOPlayerHandle h1, h2;
+
+        p1.size = p2.size = sizeof(GGPOPlayer);
+        p1.type = GGPO_PLAYERTYPE_LOCAL;            // local player
+        p2.type = GGPO_PLAYERTYPE_REMOTE;           // remote player
+        p1.player_num = 1;
+        p2.player_num = 2;
+        strcpy(p2.u.remote.ip_address, "127.0.0.1");// ip addess of the player
+        p2.u.remote.port = 7001;                    // port of that player
+        printf("%s:%d\n", p2.u.remote.ip_address, p2.u.remote.port);
+
+        result = ggpo_add_player(ggpo_, &p1,  &h1);
+        printf("GGPO Error: %d\n", result);
+        ggpo_set_frame_delay(ggpo_, h1, 2);         // only set frame delay for self
+
+        result = ggpo_add_player(ggpo_, &p2,  &h2);
+        printf("GGPO Error: %d\n", result);
+
+        printf("GGPO: connecting...\n");
     }
 
     void update_state(int f) {
@@ -31,6 +120,13 @@ public:
                                    utils::r_random(150),
                                    utils::distributed_random(chances),
                                    utils::r_random(25));
+    }
+
+    void cleanup() { //might not be used in test
+        ggpo_close_session(ggpo_);
+#if defined(__WIN32__) || defined(WIN32)
+        WSACleanup();
+#endif
     }
 
     void cycle(){
@@ -57,6 +153,10 @@ private:
     view::pSprite sprite_;
     int frame_;
     bool rollbacked_;
+
+    //networking and ggpo related
+    GGPOSession* ggpo_;
+    GGPOSessionCallbacks ggpo_callbacks_;
 };
 
 #include "App.hpp"

210132fce8eeca787ba5d4490d3e52f9748bdaa8
Maxwell <maxwellrocker@gmail.com>
2013-09-13 17:27:44 +0800

    batch generate zzml feature

--- cubeat-core/generate_puzzle.lua
+++ cubeat-core/generate_puzzle.lua
@@ -8,7 +8,11 @@ local w_            = 6
 local h_            = 11
 local chain_limit_  = tonumber(arg[1])
 local color_num_    = tonumber(arg[2])
-local de_bug_       = string_to_boolean(arg[3])
-local outfile_      = arg[4]
+local num_          = tonumber(arg[3]) if num_>10 then num_ = 10 end
+local de_bug_       = string_to_boolean(arg[4])
+local outfile_      = arg[5] or "rc/config/tmp/puzzle"
 
-generate.gen(chain_limit_, w_, h_, de_bug_, color_num_, outfile_)
+for i=1,num_ do
+  filepath = outfile_ .. tostring(i) .. '.zzml'
+  generate.gen(chain_limit_, w_, h_, de_bug_, color_num_, filepath)
+end

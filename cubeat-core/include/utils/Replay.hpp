0ec9c7d3da78d8e84e6f4f00c4b22b3e4d985e2e
Johnson Lin <arch.jslin@gmail.com>
2014-08-11 17:49:27 +0800

    fleshing out Replay class. It's capable of replaying and recording functionality. Did this because I don't want made coupling more messy, all the complex code and problematic code is contained in this class. So that plugging code in other classes as less as possible

--- cubeat-core/include/utils/Replay.hpp
+++ cubeat-core/include/utils/Replay.hpp
@@ -3,25 +3,44 @@
 
 #include "utils/dictionary.hpp"
 #include "ButtonEnum.hpp"
+#include <boost/tr1/memory.hpp>
 
 namespace psc {
+
+namespace ctrl {
+class Input;
+class TimerDispatcher;
+typedef std::tr1::shared_ptr<TimerDispatcher> pTimerDispatcher;
+typedef std::tr1::weak_ptr<TimerDispatcher> wpTimerDispatcher;
+}
+
 namespace utils {
 
 class Replay
 {
 public:
     void record_frame_time();
-    void record_input_state();
+    void record_input_state(ctrl::Input* input);
+    void set_timer_dispatcher(ctrl::wpTimerDispatcher t);
+    void set_time();
+    void set_input_for(ctrl::Input* input);
+    void replay_advance();
     void seed(int const& seed);
+    int  seed() const;
+    bool read_file(std::string const& path);
+    void toggle_recording_andor_replaying(bool const& f);
     void reset_data();
-    void toggle_enable(bool const& f);
-    bool is_enabled() const { return enabled_; }
+    bool is_recording() const { return recording_; }
+    bool is_replaying() const { return replaying_; }
 
     Replay();
 
 private:
-    bool enabled_;
+    bool recording_, replaying_;
+    bool p1t1_, p1t2_, p2t1_, p2t2_;
+    int x1_, y1_, x2_, y2_;
     map_any replay_data_;
+    ctrl::wpTimerDispatcher game_timer_;
 };
 
 } //utils

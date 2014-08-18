048b92f332979cc2a790c25f2ab5b92c282d05e7
Johnson Lin <arch.jslin@gmail.com>
2014-08-18 16:09:11 +0800

    added preload_page function specifically for preloading. moved slide_in_title and slide_out_title effects to special cases of UI effects and they are not able to be accessed from the outside.

--- cubeat-core/rc/script/ui/demo/switch/switch.lua
+++ cubeat-core/rc/script/ui/demo/switch/switch.lua
@@ -20,6 +20,10 @@ local teamname_
 local blocker_
 local transfer_
 local transfer_title_
+
+-- mainmenu bg --
+local skyblue_
+local bg_
 --
 local effect_ = {}
 local effect_count_ = 0
@@ -58,6 +62,14 @@ local function init(parent, demo)
   transfer_:set_blue(0)
   transfer_:set_green(0)
   transfer_title_ = ui.new_image{ parent=transfer_._cdata, path='title', x=0, y=0, w=512, h=512, center=true }
+  
+  skyblue_ = ui.new_image { parent = root_, path='nothing', w=1280, h=720, center=false }
+  skyblue_:set_depth(100)
+  skyblue_:set_red(0) 
+  skyblue_:set_green(200) 
+  
+  bg_ = ui.new_image{ parent=root_, path='mainmenu/bg', y=720, w=1280, h=720, center=false }
+  bg_:set_depth(50)
 end
 
 ------------------------------------------------------------
@@ -102,6 +114,7 @@ effect_.slide_out = function(effect)
   local s1 = ffi.new("v2", 0, 0)
   local e1 = ffi.new("v2", 2000, 0)
   vorig_:tween("OSine", "Pos2D", s1, e1, 1000)
+  root_:tween("OSine", "Pos2D", s1, e1, 1000)
   fade_out_blocker()
 end
 
@@ -109,6 +122,7 @@ effect_.slide_in = function(effect)
   local s1 = ffi.new("v2", -2160, 0)
   local e1 = ffi.new("v2", 0, 0)
   vorig_:tween("ISine", "Pos2D", s1, e1, 1000)
+  root_:tween("ISine", "Pos2D", s1, e1, 1000)
   fade_in_blocker()
 end
 
@@ -124,31 +138,9 @@ effect_.slide_in_to_talk = function(effect)
 end
 
 effect_.slide_out_title = function(effect)
-  local s2 = ffi.new("v2", 640, 150)
-  local e2 = ffi.new("v2", 640, -200)
-  title_:tween("ISine", "Pos2D", s2, e2, 500)
-
-  local s3 = ffi.new("v2", 1000, 280)
-  local e3 = ffi.new("v2", 1000, 780)
-  demobuild_:tween("ISine", "Pos2D", s3, e3, 750)
-
-  local s4 = ffi.new("v2", 640, 690)
-  local e4 = ffi.new("v2", 640, 780)
-  teamname_:tween("ISine", "Pos2D", s4, e4, 400)
 end
 
 effect_.slide_in_title = function(effect)
-  local s2 = ffi.new("v2", 640, -200)
-  local e2 = ffi.new("v2", 640, 150)
-  title_:tween("OSine", "Pos2D", s2, e2, 500)
-
-  local s3 = ffi.new("v2", 1000, 780)
-  local e3 = ffi.new("v2", 1000, 280)
-  demobuild_:tween("OElastic", "Pos2D", s3, e3, 750)
-
-  local s4 = ffi.new("v2", 640, 780)
-  local e4 = ffi.new("v2", 640, 690)
-  teamname_:tween("OSine", "Pos2D", s4, e4, 400)
 end
 
 effect_.slide_out_transfer = function(effect)
@@ -171,6 +163,44 @@ effect_.slide_in_transfer = function(effect)
   local s1 = ffi.new("v2", 640, -480)
   local e1 = ffi.new("v2", 640,  360)
   transfer_:tween("OElastic", "Pos2D", s1, e1, 2000, 0, effect.cb, 0)
+end
+
+-- Global Effect regarding the main background + title etc.
+
+local function startscreen_to_mainmenu()
+  local s2 = ffi.new("v2", 640, 150)
+  local e2 = ffi.new("v2", 640, -200)
+  title_:tween("ISine", "Pos2D", s2, e2, 500)
+
+  local s3 = ffi.new("v2", 1000, 280)
+  local e3 = ffi.new("v2", 1000, 780)
+  demobuild_:tween("ISine", "Pos2D", s3, e3, 750)
+
+  local s4 = ffi.new("v2", 640, 690)
+  local e4 = ffi.new("v2", 640, 780)
+  teamname_:tween("ISine", "Pos2D", s4, e4, 400)
+
+  local s5 = ffi.new("v2", 0, 720)
+  local e5 = ffi.new("v2", 0, 0)
+  bg_:tween("ISine", "Pos2D", s5, e5, 1000)
+end
+
+local function mainmenu_to_startscreen()
+  local s2 = ffi.new("v2", 640, -200)
+  local e2 = ffi.new("v2", 640, 150)
+  title_:tween("OSine", "Pos2D", s2, e2, 500)
+
+  local s3 = ffi.new("v2", 1000, 780)
+  local e3 = ffi.new("v2", 1000, 280)
+  demobuild_:tween("OElastic", "Pos2D", s3, e3, 750)
+
+  local s4 = ffi.new("v2", 640, 780)
+  local e4 = ffi.new("v2", 640, 690)
+  teamname_:tween("OSine", "Pos2D", s4, e4, 400)
+
+  local s5 = ffi.new("v2", 0, 0)
+  local e5 = ffi.new("v2", 0, 720)
+  bg_:tween("ISine", "Pos2D", s5, e5, 1000)
 end
 
 ------------------------------------------------------------
@@ -196,15 +226,30 @@ local function remove_to_be_delete()
 end
 
 -- added data parameter to customize the same menu with different options
+local function preload_page(name)
+  local p = require ('rc/script/ui/demo/'..name..'/'..name)
+  page_obj_ = p.init(game_demo_, vorig_)
+  hide_page_obj()
+  view.debug_hack()
+end
+
 local function load_page(name, effect, data)
   remove_to_be_delete()
   hide_page_obj()
 
+  local prev_page = current_page_
+  
   to_be_delete_ = page_obj_
   local p = require ('rc/script/ui/demo/'..name..'/'..name)
   data_ = data
   current_page_ = 'rc/script/ui/demo/'..name..'/'..name
   page_obj_ = p.init(game_demo_, vorig_, data)
+  
+  if name == 'mainmenu' and prev_page == 'rc/script/ui/demo/startscreen/startscreen' then
+    startscreen_to_mainmenu()
+  elseif name == 'startscreen' and prev_page == 'rc/script/ui/demo/mainmenu/mainmenu' then
+    mainmenu_to_startscreen()
+  end
   
   show_effect(effect)
 
@@ -240,6 +285,7 @@ return {
   ----
   show_effect   = show_effect,
   ----
+  preload_page  = preload_page,
   load_page     = load_page,
   refresh_page  = refresh_page,
   get_page_obj  = get_page_obj

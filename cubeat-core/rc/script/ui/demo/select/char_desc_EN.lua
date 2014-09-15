e509d7c225ddb165da5dd0fe34f06bf321d5883e
Johnson Lin <arch.jslin@gmail.com>
2014-09-16 07:00:34 +0800

    update text

--- cubeat-core/rc/script/ui/demo/select/char_desc_EN.lua
+++ cubeat-core/rc/script/ui/demo/select/char_desc_EN.lua
@@ -1,8 +1,8 @@
 return {
   'Less heat build-up\nShorter countdown time',
   'Longer countdown time\nLonger overheat down time',
-  'More green and blue when hit\nLess auto restore',
+  'More green/blue when hit\nLess auto restore',
   'Much more red when hit\nMore heat build-up',
-  'Less yellow\nNo weakness',
-  'More auto restore\nHeat cool down slower'
+  'Less yellow when hit\nNo weakness',
+  'More auto restore when hit\nHeat cool down slower'
 }

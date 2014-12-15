a4d5075e6e8548d020839f3c35bd1400cdc08ab7
Maxwell <maxwellrocker@gmail.com>
2014-12-15 16:03:40 +0800

    add left&right btn to change achievement page style

--- cubeat-core/rc/script/ui/demo/achievement/achievement.lua
+++ cubeat-core/rc/script/ui/demo/achievement/achievement.lua
@@ -11,63 +11,46 @@ local function init(demo, parent)
   
   root_ = view.new_sprite("blahblah", parent, 0, 0, true)
   root_:set_pos(0, 0)
-  menu.bt_group1  = ui.new_text{ parent=root_, x=50, y= 60, size=32, title='Group 1' }
-  menu.bt_group2  = ui.new_text{ parent=root_, x=50, y=120, size=32, title='Group 2' }
-  menu.bt_group3  = ui.new_text{ parent=root_, x=50, y=180, size=32, title='Group 3' }
-  menu.bt_group4  = ui.new_text{ parent=root_, x=50, y=240, size=32, title='Group 4' }
-  menu.bt_group5  = ui.new_text{ parent=root_, x=50, y=300, size=32, title='Group 5' }
-  menu.btn_back   = ui.new_text{ parent=root_, x=50, y=600, size=32, title='go back' }
-  menu.bt_group1:set_scale(1.5)
-  menu.bt_group2:set_scale(1.5)
-  menu.bt_group3:set_scale(1.5)
-  menu.bt_group4:set_scale(1.5)
-  menu.bt_group5:set_scale(1.5)
+  
+  menu.left_btn   = ui.new_text{ parent=root_, x=200, y=300, size=32, title='<<' }
+  menu.left_btn:set_scale(1.5)
+  
+  menu.right_btn  = ui.new_text{ parent=root_, x=view.GET_SCREEN_W()-200, y=300, size=32, title='>>' }
+  menu.right_btn:set_scale(1.5)
+  
+  menu.btn_back = ui.new_text{ parent=root_, x=10, y=view.GET_SCREEN_H()-60, size=32, title='< go back' }
   menu.btn_back:set_scale(1.5)
   
   for i=1,9 do
-    menu["achevement_" .. tostring(   i)] = ui.new_text{ parent=root_, x=400, y= 20+i*40, size=32, title='Achievement ' .. tostring(   i) }
-    menu["achevement_" .. tostring(10+i)] = ui.new_text{ parent=root_, x=400, y= 20+i*40, size=32, title='Achievement ' .. tostring(10+i), visible=false }
-    menu["achevement_" .. tostring(20+i)] = ui.new_text{ parent=root_, x=400, y= 20+i*40, size=32, title='Achievement ' .. tostring(20+i), visible=false }
-    menu["achevement_" .. tostring(30+i)] = ui.new_text{ parent=root_, x=400, y= 20+i*40, size=32, title='Achievement ' .. tostring(30+i), visible=false }
-    menu["achevement_" .. tostring(40+i)] = ui.new_text{ parent=root_, x=400, y= 20+i*40, size=32, title='Achievement ' .. tostring(40+i), visible=false }
+    menu["achevement_" .. tostring(   i)] = ui.new_text{ parent=root_, x=640, y= 200+i*40, size=32, title='Achievement ' .. tostring(   i), center=true }
+    menu["achevement_" .. tostring(10+i)] = ui.new_text{ parent=root_, x=640, y= 200+i*40, size=32, title='Achievement ' .. tostring(10+i), center=true, visible=false }
+    menu["achevement_" .. tostring(20+i)] = ui.new_text{ parent=root_, x=640, y= 200+i*40, size=32, title='Achievement ' .. tostring(20+i), center=true, visible=false }
+    menu["achevement_" .. tostring(30+i)] = ui.new_text{ parent=root_, x=640, y= 200+i*40, size=32, title='Achievement ' .. tostring(30+i), center=true, visible=false }
+    menu["achevement_" .. tostring(40+i)] = ui.new_text{ parent=root_, x=640, y= 200+i*40, size=32, title='Achievement ' .. tostring(40+i), center=true, visible=false }
   end
   
   local current_group = 0
   
-  menu.bt_group1:on_press(function(self)
+  menu.left_btn:on_press(function(self)
     for i=1,9 do
       menu["achevement_" .. tostring(current_group+i)]:set_visible(false)
-      menu["achevement_" .. tostring(i)]:set_visible(true)
     end
-    current_group = 0
+    current_group = current_group - 10
+    if current_group<0 then current_group=40 end
+    for i=1,9 do
+      menu["achevement_" .. tostring(current_group+i)]:set_visible(true)
+    end
   end)
-  menu.bt_group2:on_press(function(self)
+  
+  menu.right_btn:on_press(function(self)
     for i=1,9 do
       menu["achevement_" .. tostring(current_group+i)]:set_visible(false)
-      menu["achevement_" .. tostring(10+i)]:set_visible(true)
     end
-    current_group = 10
-  end)
-  menu.bt_group3:on_press(function(self)
+    current_group = current_group + 10
+    if current_group>40 then current_group=0 end
     for i=1,9 do
-      menu["achevement_" .. tostring(current_group+i)]:set_visible(false)
-      menu["achevement_" .. tostring(20+i)]:set_visible(true)
+      menu["achevement_" .. tostring(current_group+i)]:set_visible(true)
     end
-    current_group = 20
-  end)
-  menu.bt_group4:on_press(function(self)
-    for i=1,9 do
-      menu["achevement_" .. tostring(current_group+i)]:set_visible(false)
-      menu["achevement_" .. tostring(30+i)]:set_visible(true)
-    end
-    current_group = 30
-  end)
-  menu.bt_group5:on_press(function(self)
-    for i=1,9 do
-      menu["achevement_" .. tostring(current_group+i)]:set_visible(false)
-      menu["achevement_" .. tostring(40+i)]:set_visible(true)
-    end
-    current_group = 40
   end)
   
   menu.btn_back:on_press(function(self)

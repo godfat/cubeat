08d09fbcb2e49d91cf582c5b81454f9c8d9fa5cf
Maxwell <maxwellrocker@gmail.com>
2014-12-17 14:51:54 +0800

    adjust achievement & optionmenu page

--- cubeat-core/rc/script/ui/demo/achievement/achievement.lua
+++ cubeat-core/rc/script/ui/demo/achievement/achievement.lua
@@ -12,14 +12,20 @@ local function init(demo, parent)
   root_ = view.new_sprite("blahblah", parent, 0, 0, true)
   root_:set_pos(0, 0)
   
+  menu.square = ui.new_image{ parent=root_, path='area_rect', x=(view.GET_SCREEN_W()-400)/2, y=100, w=400, h=600 }
+  
   menu.left_btn   = ui.new_text{ parent=root_, x=200, y=300, size=32, title='<<' }
   menu.left_btn:set_scale(1.5)
   
   menu.right_btn  = ui.new_text{ parent=root_, x=view.GET_SCREEN_W()-200, y=300, size=32, title='>>' }
   menu.right_btn:set_scale(1.5)
   
-  menu.btn_back = ui.new_text{ parent=root_, x=10, y=view.GET_SCREEN_H()-60, size=32, title='< go back' }
-  menu.btn_back:set_scale(1.5)
+  menu.btn_back = ui.new_text{ parent=root_, x=view.GET_SCREEN_W()/2, y=640, size=40, title='back', depth=-200, center=true }
+  
+  for i=0,4 do
+    menu["title_" .. tostring(i*10)] = ui.new_text{ parent=root_, x=640, y=180, size=40, title="Title " .. tostring(i+1), center=true }
+    if i~=0 then menu["title_" .. tostring(i*10)]:set_visible(false) end
+  end
   
   for i=1,9 do
     menu["achevement_" .. tostring(   i)] = ui.new_text{ parent=root_, x=640, y= 200+i*40, size=32, title='Achievement ' .. tostring(   i), center=true }
@@ -35,22 +41,30 @@ local function init(demo, parent)
     for i=1,9 do
       menu["achevement_" .. tostring(current_group+i)]:set_visible(false)
     end
+    menu["title_" .. tostring(current_group)]:set_visible(false)
+    
     current_group = current_group - 10
+    
     if current_group<0 then current_group=40 end
     for i=1,9 do
       menu["achevement_" .. tostring(current_group+i)]:set_visible(true)
     end
+    menu["title_" .. tostring(current_group)]:set_visible(true)
   end)
   
   menu.right_btn:on_press(function(self)
     for i=1,9 do
       menu["achevement_" .. tostring(current_group+i)]:set_visible(false)
     end
+    menu["title_" .. tostring(current_group)]:set_visible(false)
+    
     current_group = current_group + 10
+    
     if current_group>40 then current_group=0 end
     for i=1,9 do
       menu["achevement_" .. tostring(current_group+i)]:set_visible(true)
     end
+    menu["title_" .. tostring(current_group)]:set_visible(true)
   end)
   
   menu.btn_back:on_press(function(self)
--- cubeat-core/rc/script/ui/demo/optionmenu/optionmenu.lua
+++ cubeat-core/rc/script/ui/demo/optionmenu/optionmenu.lua
@@ -10,10 +10,13 @@ local function init(demo, parent)
   local menu = {}
   
   root_ = view.new_sprite("blahblah", parent, 0, 0, true)
-  root_:set_pos(480, 300)
+  local root_x = 200
+  local root_y = 60
+  root_:set_pos(root_x, root_y)
   
-  menu.btn_back  = ui.new_text{ parent=root_, x=0, y=180, size=32, title='go back' }
-  menu.btn_back:set_scale(1.5)
+  menu.square = ui.new_image{ parent=root_, path='area_rect', x=0, y=0, w=view.GET_SCREEN_W()-(root_x*2), h=view.GET_SCREEN_H()-(root_y*2) }
+  
+  menu.btn_back  = ui.new_text{ parent=root_, x=(view.GET_SCREEN_W()/2)-root_x, y=view.GET_SCREEN_H()-140-root_y, size=40, title='back', center=true, depth=-200 }
   menu.btn_back:on_press(function(self)
     switch.load_page('extramenu')
   end)

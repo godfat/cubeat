ac4909be31b009549eaec76cdb27161230c52c72
Johnson Lin <arch.jslin@gmail.com>
2015-05-27 18:00:25 +0800

    add blocker_left_ and right

--- cubeat-core/rc/script/ui/demo/vsend/vsend.lua
+++ cubeat-core/rc/script/ui/demo/vsend/vsend.lua
@@ -12,6 +12,8 @@ local center_x = screen_w_/2
 local center_y = screen_h_/2
 
 local blocker_
+local blocker_left_
+local blocker_right_
 local win_t_
 local lose_t_
 local end_text_
@@ -21,6 +23,8 @@ local char_big2_
 
 local function hide()
   blocker_:set_visible(false)
+  blocker_left_:set_visible(false)
+  blocker_right_:set_visible(false)
   win_t_:set_visible(false)
   lose_t_:set_visible(false)
   end_text_:set_visible(false)
@@ -31,9 +35,14 @@ end
 
 local function create(scene)
   
-  blocker_  = ui.new_image{ parent=scene, path="blocker", x=center_x, y=center_y, w=screen_w_, h=384, depth=-10, alpha=144, center=true }
+  blocker_  = ui.new_image{ parent=scene, path="blocker", x=center_x, y=center_y + 210, w=300, h=214, depth=-10, alpha=168, center=true }
   blocker_:set_gradient_diffuse(0)
   
+  blocker_left_ = ui.new_image{ parent=scene, path="blocker", x=95, y=42, w=384, h=640, depth=-10, alpha=96 }
+  blocker_left_:set_gradient_diffuse(0)
+  blocker_right_= ui.new_image{ parent=scene, path="blocker", x=802, y=42, w=384, h=640, depth=-10, alpha=96 }
+  blocker_right_:set_gradient_diffuse(0)
+ 
   win_t_  = ui.new_image { parent=scene, path="win" , x=0, y=0, w=384, h=192, depth=-450, center=true }
   lose_t_ = ui.new_image { parent=scene, path="lose", x=0, y=0, w=384, h=192, depth=-450, center=true }
   
@@ -42,8 +51,8 @@ local function create(scene)
   
   --char_big1_ = ui.new_image{ parent=scene, path="char1_new/glad", x=0, y=0, w=432, h=648, center=true }
   --char_big2_ = ui.new_image{ parent=scene, path="char1_new/glad", x=0, y=0, w=432, h=648, center=true }
-  char_big1_ = ui.new_image{ parent=scene, path="char1_new/glad", x=0, y=0, w=435, h=870, center=true }
-  char_big2_ = ui.new_image{ parent=scene, path="char1_new/glad", x=0, y=0, w=435, h=870, center=true }
+  char_big1_ = ui.new_image{ parent=scene, path="char1_new/glad", x=0, y=0, w=435, h=870, center=true, depth=-20 }
+  char_big2_ = ui.new_image{ parent=scene, path="char1_new/glad", x=0, y=0, w=435, h=870, center=true, depth=-20 }
   char_big2_:texture_flipH()
   --
   
@@ -55,7 +64,14 @@ local function show(demo, game_mode, submode, p1_win)
   blocker_:set_alpha(0)
   blocker_:tween("Linear", "Alpha", 0, 144, 500, 0, nil, 1500)
   blocker_:set_visible(true)
-  blocker_:set_pos(center_x, center_y)
+  blocker_:set_pos(center_x, center_y + 120)
+  
+  blocker_left_:set_alpha(0)
+  blocker_left_:set_visible(true)
+  blocker_left_:tween("Linear", "Alpha", 0, 144, 500, 0, nil, 1500)
+  blocker_right_:set_alpha(0)
+  blocker_right_:set_visible(true)
+  blocker_right_:tween("Linear", "Alpha", 0, 144, 500, 0, nil, 1500)
   
   win_t_:set_visible(true)
   win_t_:set_scale(0,0)
@@ -69,8 +85,8 @@ local function show(demo, game_mode, submode, p1_win)
   
   -- if p1 lose
   if p1_win == false then
-    lose_t_:set_pos(pos1.x - 35, pos1.y - 50)
-    win_t_:set_pos(pos2.x  + 35, pos2.y - 50)
+    lose_t_:set_pos(pos1.x - 35, pos1.y + 10)
+    win_t_:set_pos(pos2.x  + 35, pos2.y + 10)
 
     char_big_filename1 = char_big_filename1 .. ("/sad")
     char_big_filename2 = char_big_filename2 .. ("/glad") 
@@ -84,8 +100,8 @@ local function show(demo, game_mode, submode, p1_win)
       demo:play_sound("3/3c/win.wav")
     end
   else 
-    lose_t_:set_pos(pos2.x + 35, pos2.y - 50)
-    win_t_:set_pos(pos1.x  - 35, pos1.y - 50)
+    lose_t_:set_pos(pos2.x + 35, pos2.y + 10)
+    win_t_:set_pos(pos1.x  - 35, pos1.y + 10)
 
     char_big_filename1 = char_big_filename1 .. ("/glad")
     char_big_filename2 = char_big_filename2 .. ("/sad")
@@ -98,10 +114,10 @@ local function show(demo, game_mode, submode, p1_win)
   char_big1_:set_texture(char_big_filename1)
   char_big2_:set_texture(char_big_filename2)
   --char_big2_:texture_flipH()
-  local s1 = ffi.new("v2", pos1.x-32, -pos1.y+64)
-  local e1 = ffi.new("v2", pos1.x-64, pos1.y)
-  local s2 = ffi.new("v2", pos2.x+32, -pos2.y+64)
-  local e2 = ffi.new("v2", pos2.x+64, pos2.y)
+  local s1 = ffi.new("v2", pos1.x-48, -pos1.y+64)
+  local e1 = ffi.new("v2", pos1.x-48, pos1.y)
+  local s2 = ffi.new("v2", pos2.x+48, -pos2.y+64)
+  local e2 = ffi.new("v2", pos2.x+48, pos2.y)
   char_big1_:set_visible(true)
   char_big2_:set_visible(true)
   char_big1_:tween("OBounce", "Pos2D", s1, e1, 1500, 0, nil, 0)
@@ -123,7 +139,7 @@ local function show(demo, game_mode, submode, p1_win)
   else
     end_text_:change_text("Rematch")
   end
-  end_text_:set_pos(center_x - 120, center_y + 110)
+  end_text_:set_pos(center_x , center_y + 80)
   end_text_:set_alpha(0)
   end_text_:set_scale(1.3)
   end_text_:set_depth(-450)
@@ -131,7 +147,7 @@ local function show(demo, game_mode, submode, p1_win)
   end_text2_:set_visible(true)
   end_text2_:set_color(255, 255, 255)
   end_text2_:change_text("Leave")
-  end_text2_:set_pos(center_x + 120, center_y + 110)
+  end_text2_:set_pos(center_x , center_y + 160)
   end_text2_:set_alpha(0)
   end_text2_:set_scale(1.3)
   end_text2_:set_depth(-450)

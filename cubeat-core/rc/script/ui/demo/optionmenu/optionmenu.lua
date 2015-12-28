5c51f3b2e10a64002e43b00ec8cd10490a150e82
Maxwell <maxwellrocker@gmail.com>
2015-12-28 15:56:53 +0800

    optionmenu use image9s

--- cubeat-core/rc/script/ui/demo/optionmenu/optionmenu.lua
+++ cubeat-core/rc/script/ui/demo/optionmenu/optionmenu.lua
@@ -9,27 +9,34 @@ local root_
 local function init(demo, parent)
   local menu = {}
   
+  local c_w = view.GET_SCREEN_W()/2
+  local c_h = view.GET_SCREEN_H()/2
+  
   root_ = view.new_sprite("blahblah", parent, 0, 0, true)
-  local root_x = 200
-  local root_y = 60
-  root_:set_pos(root_x, root_y)
+  root_:set_pos(c_w, c_h)
+  --local root_x = 200-440
+  --local root_y = 60-300
+  --root_:set_pos(root_x, root_y)
   
-  menu.square = ui.new_image{ parent=root_, path='area_rect', x=0, y=0, w=view.GET_SCREEN_W()-(root_x*2), h=view.GET_SCREEN_H()-(root_y*2) }
+  menu.bg = ui.new_image9s{ parent=root_, path='textarea2', x=0, y=0, 
+                            w=600, h=480, w1=34, w2=32, h1=38, h2=35, center=true }
   
-  menu.title = ui.new_text{ parent=root_, x=(view.GET_SCREEN_W()/2)-root_x, y=100, title='Option', size=40, center=true }
+  --menu.square = ui.new_image{ parent=root_, path='area_rect', x=0, y=0, w=view.GET_SCREEN_W()-(root_x*2), h=view.GET_SCREEN_H()-(root_y*2) }
   
-  menu.music_title = ui.new_text{ parent=root_, title='Music', x=100, y=200, size=32 }
-  menu.music_scrollbar = ui.new_scrollbar{ parent=root_, x=260, y=200, range=100, index=50 }
+  menu.title = ui.new_text{ parent=root_, x=0, y=-160, title='Option', size=40, depth=-60, center=true }
   
-  menu.sound_title = ui.new_text{ parent=root_, title='Sound', x=100, y=260, size=32 }
-  menu.sound_scrollbar = ui.new_scrollbar{ parent=root_, x=260, y=260, range=100, index=50 }
+  menu.music_title = ui.new_text{ parent=root_, title='Music', x=-220, y=-80, depth=-60, size=32 }
+  menu.music_scrollbar = ui.new_scrollbar{ parent=root_, x=-60, y=-80, range=100, depth=-60, index=50 }
   
-  menu.ratio_1 = ui.new_ratio{ parent=root_, x=100, y=320, title='Effect 1' }
+  menu.sound_title = ui.new_text{ parent=root_, title='Sound', x=-220, y=-20, depth=-60, size=32 }
+  menu.sound_scrollbar = ui.new_scrollbar{ parent=root_, x=-60, y=-20, range=100, depth=-60, index=50 }
+  
+  menu.ratio_1 = ui.new_ratio{ parent=root_, x=-220, y=40, depth=-60, title='Effect 1' }
   menu.ratio_1:on_press(function(self) end)
-  menu.ratio_2 = ui.new_ratio{ parent=root_, x=100, y=380, title='Effect 2' }
+  menu.ratio_2 = ui.new_ratio{ parent=root_, x=-220, y=100, depth=-60, title='Effect 2' }
   menu.ratio_2:on_press(function(self) end)
   
-  menu.btn_back  = ui.new_text{ parent=root_, x=(view.GET_SCREEN_W()/2)-root_x, y=view.GET_SCREEN_H()-140-root_y, size=40, title='back', center=true, depth=-200 }
+  menu.btn_back  = ui.new_text{ parent=root_, x=0, y=180, size=40, title='back', center=true, depth=-200 }
   menu.btn_back:on_press(function(self)
     switch.load_page('extramenu')
   end)
